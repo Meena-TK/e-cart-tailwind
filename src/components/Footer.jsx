d79338de32bbd21c1711e3c28192141d8c3ef27f
Meena-TK <meenaparanthal@gmail.com>
2025-01-15 16:27:10 +0530

    second commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div style={{height:'300px'}} className='bg-violet-600 mt-6 p-5 w-full text-white'> 
+      <div class="flex justify-between">
+        {/* intro */}
+        <div style={{width:'400px'}} >
+         <Link className='text-2xl font-bold' to={'/'}><i className='fa-solid fa-truck-fast me-1'></i>Daily Cart
+               </Link>
+            <p class="mb-2">Designed and built with all the love in the world by the  with the help of our contributors</p>
+            <p>Code licensed docs CC BY 3.0 </p>
+            <p>Currently v5.3.3.</p>
+        </div>
+
+        {/* Link */}
+        <div className='flex flex-col'>
+          <h5 className='font-bold'>Links</h5>
+          <Link to={'/'} style={{textDecoration:'none', color:'white'}}>Home Page</Link>
+          <Link to={'/cart'} style={{textDecoration:'none', color:'white'}}>Cart Page</Link>
+          <Link to={'/wishlist'} style={{textDecoration:'none', color:'white'}}>Wishlist Page</Link>
+        </div>
+
+        {/* Guides */}
+        <div className='flex flex-col'>
+          <h5 className='font-bold'>Guides</h5>
+          <a style={{textDecoration:'none', color:'white'}} target='_blank' href="https://react.dev/">React</a>
+          <a style={{textDecoration:'none', color:'white'}} target='_blank' href="https://react-bootstrap.netlify.app/">React Bootstrap</a>
+          <a style={{textDecoration:'none', color:'white'}} target='_blank' href="https://www.npmjs.com/package/react-router-dom">React Router</a>
+        </div>
+
+        {/* Contacts */}
+        <div className='flex flex-col'>
+          <h5 className='font-bold'>Contact Us</h5>
+          <div className='flex justify-between mt-3'>
+            <input type="text" placeholder='Email....' className='form-control me-2'  />
+            <button className='btn btn-info'><i class="fa-solid fa-arrow-right"></i></button>
+          </div>
+          <div className='flex justify-between mt-3'>
+            <a style={{textDecoration:'none', color:'white'}} target='_blank' href="https://en.wikipedia.org/wiki/Twitter" ><i class="fa-brands fa-twitter"></i></a>
+            <a style={{textDecoration:'none', color:'white'}} target='_blank' href="https://www.instagram.com/" ><i class="fa-brands fa-instagram"></i></a>
+            <a style={{textDecoration:'none', color:'white'}} target='_blank' href="https://www.facebook.com/" ><i class="fa-brands fa-facebook"></i></a>
+            <a style={{textDecoration:'none', color:'white'}} target='_blank' href="https://www.linkedin.com" ><i class="fa-brands fa-linkedin"></i></a>
+            <a style={{textDecoration:'none', color:'white'}} target='_blank' href="https://github.com/" ><i class="fa-brands fa-github"></i></a>
+            <a style={{textDecoration:'none', color:'white'}} target='_blank' href="/" ><i class="fa-solid fa-phone"></i></a>
+          </div>
+        </div>
+        
+      
+      </div>
+      <p className='text-center mt-3'>Copyright &copy May 2024 Batch, E Cart. Built with React Redux.</p>
+    </div>
   )
 }
 
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Header from '../components/Header'
 
 const Wishlist = () => {
   return (
-    <div>Wishlist</div>
+    <>
+    <Header/>
+    <div style={{paddingTop:'100px'}} className='px-5'>
+      <>
+        <h1 className='text-red-600 text-4xl font-bold'>My Wishlist</h1>
+        <div className='grid grid-cols-4 gap-4'>
+        <div className='rounded border p-2 shadow'>
+          <img height={'200px'} width={'100%'} src="https://img.freepik.com/free-vector/shopping-supermarket-cart-with-grocery-pictogram_1284-11697.jpg?semt=ais_hybrid" alt="" />
+          <div className='text-center'>
+            <h3 className='text-xl font-bold'>Title</h3>
+            <div className='flex justify-evenly mt-3'>
+              <button className='text-xl'><i className='fa-solid fa-heart-circle-xmark text-red-500'></i></button>
+              <button className='text-xl'><i className='fa-solid fa-cart-plus text-green-700'></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+      </>
+    </div>
+    </>
   )
 }
 
